a01c0cf9d33ff6d6a2cfaaf2f8deec08e7bbb4bf
NavyaRaju26 <nr0642387@gmail.com>
2025-09-28 12:42:01 +0530

    Create P29.php

---
+++ P29.php
@@ -0,0 +1,6 @@
+php
+echo rand(1, 10);        // Random number
+echo abs(-5);            // 5
+echo str_replace("a", "e", "cat"); // cet
+echo pi();               // 3.14159...
+echo ceil(4.3);          // 5
