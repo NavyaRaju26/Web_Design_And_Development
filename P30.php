63590a2bd8065d4d9cb34cc23c68be6799d2d65c
NavyaRaju26 <nr0642387@gmail.com>
2025-09-28 12:42:44 +0530

    Create P30.php

---
+++ P30.php
@@ -0,0 +1,8 @@
+php
+<?php
+function generatePassword($length = 8) {
+  $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%';
+  return substr(str_shuffle($chars), 0, $length);
+}
+echo generatePassword();
+?>
